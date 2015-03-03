8971385386fc5869a4602152bb29742c2ba38d14
Matt Stratton <matt.stratton@gmail.com>
2015-03-02 19:28:18 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'mattstratton@chef.io'
 license          'all_rights'
 description      'Installs/Configures pg_base'
 long_description 'Installs/Configures pg_base'
-version          '0.1.0'
+version          '0.1.1'
 
 depends           'chef-client', '= 4.2.4'
